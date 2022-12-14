3165cb51d086f1203f78fc4dbdb59fd97afa3270
Onkur Lal <onkur24lal@gmail.com>
2022-12-14 18:27:16 -0500

    simplified login button

--- src/components/LoginButton.jsx
+++ src/components/LoginButton.jsx
@@ -2,22 +2,15 @@ import React, {useState} from 'react';
 import './LoginButton.css'
 
 const LoginButton = () =>{
-    const [login, setLogin] = useState('Login',);
-    const [loginMessage, setLoginMessage] = useState('User is logged out.');
-    const handleLogin = (e) => {
-        if (e.target.name === 'userLogin'){
-            setLogin(prev => prev === 'Login' ? 'Logout' : 'Login');
-            setLoginMessage(prev => prev === 'User is logged out.' ? 'User is logged in.' : 'User is logged out.')
-        }
-    }
-    return(
-    <>
-        <div className="loginButton">
-            <button onClick = {handleLogin} name = 'userLogin'>{login}</button>
-            <p>{loginMessage}</p>
-        </div>
-    </>
-    )
+    const [user, setUser] = useState(false);
+    return (
+      <div className='loginButton'>
+        <button onClick={() => setUser(!user)}>
+          {user ? "Log out" : "Log in"}
+        </button>
+        <p>You are {user ? "Logged in" : "Logged out"}</p>
+      </div>
+      )
 }
 
 export default LoginButton;
