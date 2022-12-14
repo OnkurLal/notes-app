ab08d0927bcfdc7daa75b49c5563533c977c4098
Onkur Lal <onkur24lal@gmail.com>
2022-12-13 23:07:27 -0500

    example of counter

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,12 +1,27 @@
-import React from "react";
-import CheckListItem from './CheckListItem'
+import React, {useState} from "react";
+import CheckListItem from './CheckListItem';
 import notes from "../notesData";
 
 const Content = () => {
-    const checkListItem= notes.map(item => <CheckListItem data = {item}/>)
+    const [notesData, setNotesData] = useState(notes);
+    // const [firstName, setFirstName] = useState('Onkur');
+    const checkListItem= notesData.map(item => <CheckListItem data = {item}/>)
+    const [count, setCount] = useState(0);
+    // const handlePlusButton = ()=> setCount(count + 1)
+    // const handleMinusButton = ()=> setCount(count - 1)
+    const handleClick = (e) => {
+        e.target.name === 'plus' ? setCount(count + 1) : setCount(count - 1)
+    }
     return (
-        checkListItem
+        <>
+        {/* <div className = 'counter'> */}
+        {/* <button onClick= {handleClick} name='plus'>+</button>
+        <h3>Count: {count}</h3>
+        <button onClick={handleClick}>-</button> */}
+        {/* <div><h1>{firstName}</h1></div> */}
+         {checkListItem}
+         {/* </div> */}
+        </>
     )
 }
-
 export default Content;
