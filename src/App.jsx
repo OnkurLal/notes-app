4f0cea2d82994c5a6ae05c485bbb943e67ac8de9
Onkur Lal <onkur24lal@gmail.com>
2022-12-20 08:24:43 -0500

    updated for netlify

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css'
-import Header from './components/header'
-import Footer from './components/footer'
-import Content from './components/content'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import Content from './components/Content'
 import LoginButton from './components/LoginButton'
 import * as React from 'react';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
